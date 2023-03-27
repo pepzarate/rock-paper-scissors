2311371838e6110dfe50810ff9a058639482056d
Jose Zarate <pepzarate@gmail.com>
2023-03-27 10:07:50 -0600

    finish v1.1.0

--- scripts.js
+++ scripts.js
@@ -1,29 +1,72 @@
 'use strict'
+
 let humanWins = 0;
 let computerWins = 0;
 const options = document.getElementById('options')
 const container = document.querySelector('#computerSelection')
+const containerScore = document.getElementById('score')
+const playerWinsContainer = document.getElementById('playerWins')
+let playerSelection = ''
+let computerSelection = ''
 
 options.addEventListener("click", e => {
+    playerSelection = e.target.id;
+    computerSelection = getComputerChoice();
+    
     validateGame(humanWins, computerWins)
-     
-    let playerSelection = e.target.id;
-    let computerSelection = getComputerChoice();
     insertComputerSelection(computerSelection, container)
+
     game(playerSelection,computerSelection)
+    printScore(containerScore)
     console.log(`Human: ${humanWins}`)
     console.log(`Computer: ${computerWins}`)
     if(validateGame(humanWins, computerWins)) {
         console.log("game finished")
+        printGameFinish()
         if(humanWins === 5){
             console.log("You Won This!")
         } else console.log("Computer Won This!")
     }
 })
 
+const printScore = containerScore => {
+    const h2 = document.createElement('h2')
+    h2.textContent = "Score"
+
+    while(containerScore.hasChildNodes()) {
+        containerScore.removeChild(containerScore.lastChild)
+    }
+
+    let p1 = document.createElement('p')
+    let p2 = document.createElement('p')
+    p1.classList.add('center')
+    p2.classList.add('center')
+    p1.textContent = `Human(You): ${humanWins}`
+    p2.textContent = `Computer: ${computerWins}`
+
+    containerScore.appendChild(h2)
+    containerScore.appendChild(p1)
+    containerScore.appendChild(p2)
+}
+
 const insertComputerSelection = (computerSelection, container) => {
-    const content = document.createElement("p");
-    content.textContent = computerSelection;
+    const content = document.createElement("i");
+    const h2 = document.createElement("h2")
+    let icon = ''
+
+    while(container.hasChildNodes()) {
+        container.removeChild(container.lastChild)
+    }
+
+    if(computerSelection === 'rock') icon = 'fa-hand-back-fist' 
+    else if(computerSelection === 'paper') icon = 'fa-hand'
+    else icon = 'fa-hand-scissors'
+
+    content.classList.add('option', 'fa-regular', `${icon}`);
+
+    h2.classList.add('options__title')
+    h2.textContent = 'CPU has made his choice'
+    container.appendChild(h2)
     container.appendChild(content);
 }
 
@@ -61,6 +104,7 @@ const game = (playerSelection, computerSelection) => {
     console.log(`You have played: ${playerSelection}`)  
     console.log("Computer choice: " + computerSelection)
     const result = playRound(playerSelection, computerSelection)
+    printPlayerWins(result, playerSelection, computerSelection)
     if(result === 1){
         console.log(`${playerSelection} beats ${computerSelection}: You Win!!`)
         humanWins++
@@ -70,5 +114,20 @@ const game = (playerSelection, computerSelection) => {
     } else {
         console.log("It's tie, repeat it")        
     }
+}
+
+const printPlayerWins = (result, playerSelection, computerSelection) =>{
+    const h2 = document.createElement('h2')
+
+    while(playerWinsContainer.hasChildNodes()) {
+        playerWinsContainer.removeChild(playerWinsContainer.lastChild)
+    }
+
+    if(result === 1) h2.textContent = `${playerSelection} beats ${computerSelection}: You Win!!`;
+    if(result === 2) h2.textContent = `${computerSelection} beats ${playerSelection}: Computer wins`;
+    if(result === 0) h2.textContent = `It's tie, repeat it`;
+
+    h2.classList.add('center')
+    playerWinsContainer.appendChild(h2)
 }
 
